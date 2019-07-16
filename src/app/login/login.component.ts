42251c881aa790514d812b52c52a049f2e054745
Francisco Riorda <riorda7@gmail.com>
2019-07-16 14:26:38 -0300

    Sección 13 terminada

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+import { element } from 'protractor';
 
 declare function init_plugins();
+declare const gapi: any;
 
 
 @Component({
@@ -11,15 +16,62 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor( public router: Router) { }
+  email: string;
+  recordar: boolean = false;
+
+  auth2: any;
+
+  constructor( public router: Router, public _usuarioService: UsuarioService) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || "";
+    if (this.email.length > 1) {
+      this.recordar = true;
+    }
   }
 
-  ingresar() {
+  googleInit() {
 
-    this.router.navigate(['/dashboard']);
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '929774454272-pkh765cukpf86cts4dtbanlsllbb13e1.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin(document.getElementById('btnGoogle'));
+    });
+  }
+
+  attachSignin( element ) {
+
+    this.auth2.attachClickHandler(element, {}, (googleUser) => {
+
+      //let profile = googleUser.getBasicProfile();
+
+      let token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle(token)
+              .subscribe(() => window.location.href = '#/dashboard');
+
+    });
+  }
+
+  ingresar( forma: NgForm) {
+
+    if (forma.invalid) {
+      return;
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recordar)
+            .subscribe(correcto => this.router.navigate(['/dashboard']));
+
+    //this.router.navigate(['/dashboard']);
   }
 
 }
